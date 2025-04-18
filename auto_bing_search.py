fe168fbc8c338c71221ad0604302d0bd56ab4f76
zhugebai <104563060+zhugebai2022@users.noreply.github.com>
2025-04-18 15:56:44 +0800

    Update auto_bing_search.py

--- auto_bing_search.py
+++ auto_bing_search.py
@@ -51,6 +51,12 @@ def main():
         chrome_options = Options()
         chrome_options.add_argument("--headless=new")
         chrome_options.add_argument("--remote-allow-origins=*")
+        chrome_options.add_argument('--no-sandbox')
+        chrome_options.add_argument('--disable-dev-shm-usage')
+        chrome_options.add_argument('--disable-gpu')
+        chrome_options.add_argument('--ignore-certificate-errors')
+        chrome_options.add_argument('--ignore-ssl-errors')
+        chrome_options.add_argument('--disable-software-rasterizer')
         
         run_search(search_terms, chrome_options)
         
